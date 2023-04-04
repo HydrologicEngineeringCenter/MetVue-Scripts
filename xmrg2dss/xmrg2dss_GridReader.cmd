b71c27c93e7e86b908ef103a439ae8c0bffa776e
FHanbali <fhanbali@yahoo.com>
2023-04-04 13:13:01 -0700

    Renamed GridReader and Created a Duplicate

---
+++ xmrg2dss/xmrg2dss_GridReader.cmd
@@ -0,0 +1,64 @@
+::: ***GridReader.cmd***
+
+::: This batch file is a template for the GridReader MetVue utility.
+::: This template demonstrates displaying the built-in usage help documentation for this utility by:
+:::    1) Setting variables
+:::    2) Setting the working directory
+:::    3) Setting allowable maximum heap memory
+:::    4) Command-line execution of concatenated settings and the help argument for this utility
+
+::@echo off
+::: Turn off the console display of the remaining commands in this batch file.
+
+::: Setting for variables to be expanded at execution time rather than at parse time,
+::: When delayed expansion is in effect, variables may be referenced using !variable_name!
+::: (in addition to the normal %variable_name% )
+SETLOCAL EnableDelayedExpansion
+
+::: Use the current directory.
+::: Specifically, use the fully qualified file pathname of this batch file's location.
+PUSHD %~dp0
+
+::: Set MetVue HOME
+set mvHOME=C:/ProgramFiles/HEC/HEC-MetVue/zBuilds/v3.1/HEC-MetVue_31_Portable/HEC-MetVue_3.1
+
+::: Setting the location of the java executable.
+set java="%mvHOME%/utilities/windows/../../jre/bin/java.exe"
+
+::: Setting the allowable maximum heap memory for use by the jvm.
+set memory_setting=-Xmx4096m
+
+::: Set the default time zone for the JVM
+set gmtTz=-Duser.timezone=GMT
+
+::: Setting the classpath for running the MetVue program.
+set class_path=-cp "%mvHOME%/utilities/windows/../../metvue/modules/*;%mvHOME%/utilities/windows/../../metvue/modules/ext/*;%mvHOME%/utilities/windows/../../platform/modules/*;%mvHOME%/utilities/windows/../../platform/lib/*;%APPDATA%/HEC/HEC-MetVue/3.1.0.1221/user/modules/*"
+
+::: Setting for calling the class to perform the operation of this utility.
+set main_method=hec/metvue/base/tin/spatialUtil/GridReader
+
+::: Setting the arguments for this utility.  
+::: Use -? to refer to the built-in usage help to select from all the available arguments for this utility.
+set usage_help=-?
+set inFile=-inFile "xmrg/MSR_mpe_xmrg%%s:MMddyyyyHH%%z"
+set controlFile=-controlFile xmrg.ctrl
+set outFile=-outFile "xmrg.dss"
+set tinInputInterval=-tinInputInterval "1H"
+set sTime=-sTime "15JUL2017,000000"
+set eTime=-eTime "18JUL2017,000000"
+set prjFile=-prjFile "USA Contiguous Albers Equal Area Conic USGS_FT.prj"
+set samplingMethod=-samplingMethod inverseDistSq
+set writeProvider=-writeProvider DSS 
+set writeType=-writeType SHG
+set dssA=-dssA "A"
+set dssB=-dssB "B"
+set dssC=-dssC "PRECIPITATION"
+set dssF=-dssF "Auld"
+::: Command-line execution.
+%java% %memory_setting% %gmtTz% %class_path% %main_method% !controlFile!
+::: change back to original directory
+POPD
+
+ENDLOCAL
+
+pause
